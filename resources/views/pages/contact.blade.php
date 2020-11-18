61a9525090a64fb03cd30de6ec9f1ed5869239f2
guyms <guymsfaithful@gmail.com>
2020-11-18 02:51:54 +0100

    Update contact

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -24,7 +24,7 @@
           <div class="media">
             <span class="iconbox iconbox-md bg-primary text-white"><i class="ti-mobile"></i></span>
             <div class="media-body ml-3">
-            <h5 class="mb-0">1-800-643-4500</h5> 
+            <h5 class="mb-0">655 325 766</h5> 
             <p>Call Us (8AM-10PM)</p>
             </div>
           </div>
@@ -34,7 +34,7 @@
           <div class="media">
             <span class="iconbox iconbox-md bg-primary text-white"><i class="ti-email"></i></span>
             <div class="media-body ml-3">
-            <a href="mailto:support@echotheme.com" class="h5">support@echotheme.com</a>
+            <a href="mailto:support@echotheme.com" class="h5">guymsfaithful@gmail.com</a>
             <p>Call Us (8AM-10PM)</p>
             </div>
           </div>
@@ -44,7 +44,7 @@
           <div class="media">
             <span class="iconbox iconbox-md bg-primary text-white"><i class="ti-location-pin"></i></span>
             <div class="media-body ml-3">
-            <h5 class="mb-0">New York Avenue, USA</h5> 
+            <h5 class="mb-0">Yaounde, CAMEROON</h5> 
             <p>Unit 25 Suite 3, 925 Prospect</p>
             </div>
           </div>
